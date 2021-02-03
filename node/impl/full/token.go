be3be1bec138d795ccd22ec8c8478143f356897c
Raúl Kripalani <raul@protocol.ai>
2021-02-03 11:38:00 +0000

    implement more JSON-RPC methods.

--- node/impl/full/token.go
+++ node/impl/full/token.go
@@ -7,9 +7,11 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/token"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -86,14 +88,65 @@ func (t *TokenAPI) TokenGetSpendersOf(ctx context.Context, tokenAddr address.Add
 	return state.ApprovalsBy(holder)
 }
 
-func (t *TokenAPI) TokenTransfer(ctx context.Context, token address.Address, from, to address.Address, amount abi.TokenAmount) (cid.Cid, error) {
-	panic("implement me")
+func (t *TokenAPI) TokenTransfer(ctx context.Context, tokenAddr address.Address, from, to address.Address, amount abi.TokenAmount) (cid.Cid, error) {
+	_, err := t.StateAPI.StateGetActor(ctx, tokenAddr, types.EmptyTSK)
+	if err != nil {
+		return cid.Undef, fmt.Errorf("failed to load token actor at address %s: %w", tokenAddr, err)
+	}
+
+	return t.pushMessage(ctx, from, func(mb token.MessageBuilder) (*types.Message, error) {
+		return mb.Transfer(tokenAddr, to, amount)
+	})
 }
 
-func (t *TokenAPI) TokenTransferFrom(ctx context.Context, token address.Address, holder, from, to address.Address, amount abi.TokenAmount) (cid.Cid, error) {
-	panic("implement me")
+func (t *TokenAPI) TokenTransferFrom(ctx context.Context, tokenAddr address.Address, holder, from, to address.Address, amount abi.TokenAmount) (cid.Cid, error) {
+	_, err := t.StateAPI.StateGetActor(ctx, tokenAddr, types.EmptyTSK)
+	if err != nil {
+		return cid.Undef, fmt.Errorf("failed to load token actor at address %s: %w", tokenAddr, err)
+	}
+
+	return t.pushMessage(ctx, from, func(mb token.MessageBuilder) (*types.Message, error) {
+		return mb.TransferFrom(tokenAddr, holder, to, amount)
+	})
 }
 
-func (t *TokenAPI) TokenApprove(ctx context.Context, token address.Address, holder, spender address.Address, amount abi.TokenAmount) (cid.Cid, error) {
-	panic("implement me")
+func (t *TokenAPI) TokenApprove(ctx context.Context, tokenAddr address.Address, holder, spender address.Address, amount abi.TokenAmount) (cid.Cid, error) {
+	_, err := t.StateAPI.StateGetActor(ctx, tokenAddr, types.EmptyTSK)
+	if err != nil {
+		return cid.Undef, fmt.Errorf("failed to load token actor at address %s: %w", tokenAddr, err)
+	}
+
+	return t.pushMessage(ctx, holder, func(mb token.MessageBuilder) (*types.Message, error) {
+		return mb.TokenApprove(tokenAddr, spender, amount)
+	})
+}
+
+func (t *TokenAPI) pushMessage(ctx context.Context, from address.Address, fn func(mb token.MessageBuilder) (*types.Message, error)) (cid.Cid, error) {
+	nver, err := t.StateAPI.StateNetworkVersion(ctx, types.EmptyTSK)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	builder := token.Message(actors.VersionForNetwork(nver), from)
+	msg, err := fn(builder)
+	if err != nil {
+		return cid.Undef, fmt.Errorf("failed to build message: %w", err)
+	}
+
+	// send the message out to the network
+	smsg, err := t.MpoolAPI.MpoolPushMessage(ctx, msg, nil)
+	if err != nil {
+		return cid.Undef, fmt.Errorf("failed to push message: %w", err)
+	}
+
+	return smsg.Cid(), nil
+}
+
+func (t *TokenAPI) messageBuilder(ctx context.Context, from address.Address) (token.MessageBuilder, error) {
+	nver, err := t.StateAPI.StateNetworkVersion(ctx, types.EmptyTSK)
+	if err != nil {
+		return nil, err
+	}
+
+	return token.Message(actors.VersionForNetwork(nver), from), nil
 }
